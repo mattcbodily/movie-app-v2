eff3b4f3e6444d53ddbc09ee27878428ff1e8207
mattcbodily <mattcbodily@gmail.com>
2020-09-30 15:14:14 -0600

    Action, SciFi, and Drama movies now display

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -5,7 +5,8 @@ import './Landing.css';
 
 export default props => {
     let [sciFiMovies, setSciFiMovies] = useState([]),
-        [actionMovies, setActionMovies] = useState([]);
+        [actionMovies, setActionMovies] = useState([]),
+        [dramaMovies, setDramaMovies] = useState([]);
 
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${movieKey}&language=en-US&sort_by=popularity.desc&include_adult=false&with_genres=878`)
@@ -19,12 +20,18 @@ export default props => {
             setActionMovies(res.data.results);
         })
         .catch(err => console.log(err))
+
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${movieKey}&language=en-US&sort_by=popularity.desc&include_adult=false&with_genres=18`)
+        .then(res => {
+            setDramaMovies(res.data.results);
+        })
+        .catch(err => console.log(err))
     }, [])
 
     return (
         <section>
             <h1>Discover Movies</h1>
-            <h3>Action Movies</h3>
+            <h3>Action</h3>
             <section className='movie-flex'>
                 {actionMovies.map((movie, i) => (
                     <section key={i}>
@@ -33,9 +40,18 @@ export default props => {
                     </section>
                 ))}
             </section>
-            <h3>Sci-Fi Movies</h3>
+            <h3>Sci-Fi</h3>
             <section className='movie-flex'>
                 {sciFiMovies.map((movie, i) => (
+                    <section key={i}>
+                        <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`}/>
+                        <p>{movie.original_title}</p>
+                    </section>
+                ))}
+            </section>
+            <h3>Drama</h3>
+            <section className='movie-flex'>
+                {dramaMovies.map((movie, i) => (
                     <section key={i}>
                         <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`}/>
                         <p>{movie.original_title}</p>
